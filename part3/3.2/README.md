9ad7898e6e637d780cf9c27343d61ea29bcc53c5
Tuukka Laitinen <8557415+tuukkalai@users.noreply.github.com>
2022-01-04 13:37:14 +0200

    Done 3.2

--- part3/3.2/README.md
+++ part3/3.2/README.md
@@ -1,1 +1,9 @@
 # 3.2
+
+To build the docker container and run with `/var/run/docker.sock:/var/run/docker.sock` volume
+
+```sh
+docker build . -t dind
+docker run -it -v /var/run/docker.sock:/var/run/docker.sock dind
+```
+
--- part3/3.2/script.sh
+++ part3/3.2/script.sh
@@ -1,9 +1,16 @@
-#!/bin/bash
+#!/bin/sh
 
-git clone git@github.com:tuukkalai/simple_api.git
+rm -rf simple_api/
+
+git clone https://github.com/tuukkalai/simple_api.git
 cd simple_api
 
-docker build . -t dockerinsidedocker
-docker tag dockerinsidedocker tuukkala/dockerinsidedocker
-docker login -u {username} -p {password}
+echo 'Building docker inside docker'
+docker build . -t dind
+docker tag dind tuukkala/dockerinsidedocker
+
+echo 'Logging in to Docker Hub'
+docker login
+
+echo 'Pushing updates to Docker Hub'
 docker push tuukkala/dockerinsidedocker:latest
